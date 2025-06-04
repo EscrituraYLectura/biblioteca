d4e48f4e38f9d399fc176aa0cd29083f124b18d2
Jose de Freitas <37962411+JoseDeFreitas@users.noreply.github.com>
2025-06-04 15:23:04 +0100

    Add favicon to tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Escritura y Lectura',
   description: 'Sitio web oficial del servidor Escritura y Lectura.',
+  icon: '/favicon.ico',
 };
 
 export default function RootLayout({
