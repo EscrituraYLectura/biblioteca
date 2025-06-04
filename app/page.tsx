aa5df4ee6b81f8fb7ce8acda776537d0d74c580c
Jose de Freitas <joselocotote26@gmail.com>
2025-06-04 21:11:38 +0100

    Directly call to Google Sheets file
    Too risky, might change later to a local file approach, with the annoyance of having to re-copy the local .json file each time there is an update.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import GoogleSheetDatabase from "./components/GoogleSheetDatabase";
+
 export default function Home() {
   return (
     <main>
       <div>Biblioteca EyL</div>
+      <GoogleSheetDatabase />
     </main>
   );
 }
