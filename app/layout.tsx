d029b529809ec20b993c18cf3be62792885719b1
Jose de Freitas <37962411+JoseDeFreitas@users.noreply.github.com>
2025-06-04 15:25:06 +0100

    Fix syntax for icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Escritura y Lectura',
   description: 'Sitio web oficial del servidor Escritura y Lectura.',
-  icon: '/favicon.ico',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
