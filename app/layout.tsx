a23f4abe36555273318240eadbbb9b861aafc204
Jose de Freitas <joselocotote26@gmail.com>
2025-06-04 19:41:37 +0100

    Change site title name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Escritura y Lectura',
+  title: 'Biblioteca | Escritura y Lectura',
   description: 'Sitio web oficial del servidor Escritura y Lectura.',
   icons: {
     icon: '/favicon.ico',
