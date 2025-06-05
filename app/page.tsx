89df7627b2a830c42f2464feb3ec119ac52d5021
Jose de Freitas <joselocotote26@gmail.com>
2025-06-05 16:08:28 +0100

    Create main files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import DataExtractor from "@/components/DataExtractor";
-
 export default function Home() {
   return (
     <main>
-      <div>Biblioteca EyL</div>
-      <DataExtractor />
     </main>
   );
 }
