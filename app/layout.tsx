68d18d55a5a8c92ec4e20e4bc0f3b03558c7611a
Jose de Freitas <joselocotote26@gmail.com>
2025-06-06 23:17:13 +0100

    Change image name and add PNG format

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: 'Biblioteca | Escritura y Lectura',
   description: 'Sitio web oficial del servidor Escritura y Lectura.',
   icons: {
-    icon: '/favicon.ico',
+    icon: '/icon.ico',
   },
 };
 
