f6f50bb50e301442c17b62df9deeebcc8c69283d
Jose de Freitas <joselocotote26@gmail.com>
2025-06-10 16:48:46 +0100

    Separate navbar items

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,24 +15,31 @@ export default function NavBar() {
 
     return (
         <nav className="navbar">
-            <ul>
-                <li onClick={() => irAPagina('/buscador')}>⌕</li>
-                <li onClick={() => irAPagina('/indices')}>≡</li>
-                <li onClick={() => irAPagina('/estadisticas')}>⊞</li>
-                <li onClick={() => setMostrarPopup(true)}>ⓘ</li>
-                {mostrarPopup && (
-                    <Popup onClose={() => setMostrarPopup(false)}>
-                        <h2>Biblioteca de Escritura y Lectura</h2>
-                        <p>
-                            Sitio web de la Biblioteca de Escritura y Lectura.<br/>
-                            Creado con <a href="https://nextjs.org/" target="_blank">Next.js</a>;
-                            hosteado en <a href="https://github.com/EscrituraYLectura/biblioteca" target="_blank">GitHub Pages</a>.<br/>
-                            También puedes ver los datos en la <a href="https://docs.google.com/spreadsheets/d/1KzBwhtz-t_5i1V9vl6FdALE17SM_5Ep9sKwWG2jN-hM/edit?usp=sharing" target="_blank">base de datos</a>.<br/>
-                            El estilo es una imitación del estilo de Discord.
-                        </p>
-                    </Popup>
-                )}
-            </ul>
+            <div className="navbar-pages">
+                <ul>
+                    <li onClick={() => irAPagina('/buscador')}>⌕</li>
+                    <li onClick={() => irAPagina('/indices')}>≡</li>
+                    <li onClick={() => irAPagina('/estadisticas')}>⊞</li>
+                </ul>
+            </div>
+            <div className="navbar-info">
+                <ul>
+                    <li>✖</li>
+                    <li onClick={() => setMostrarPopup(true)}>ⓘ</li>
+                    {mostrarPopup && (
+                        <Popup onClose={() => setMostrarPopup(false)}>
+                            <h2>Biblioteca de Escritura y Lectura</h2>
+                            <p>
+                                Sitio web de la Biblioteca de Escritura y Lectura.<br/>
+                                Creado con <a href="https://nextjs.org/" target="_blank">Next.js</a>;
+                                hosteado en <a href="https://github.com/EscrituraYLectura/biblioteca" target="_blank">GitHub Pages</a>.<br/>
+                                También puedes ver los datos en la <a href="https://docs.google.com/spreadsheets/d/1KzBwhtz-t_5i1V9vl6FdALE17SM_5Ep9sKwWG2jN-hM/edit?usp=sharing" target="_blank">base de datos</a>.<br/>
+                                El estilo es una imitación del estilo de Discord.
+                            </p>
+                        </Popup>
+                    )}
+                </ul>
+            </div>
         </nav>
     );
 }
